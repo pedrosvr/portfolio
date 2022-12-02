e498ca88c0ca34a079c549e494c04ddf442d5f10
pedrosvr <jopesaraiva0@gmail.com>
2022-12-02 19:01:59 -0300

    Quarto commit

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -4,7 +4,7 @@ export default function Portfolio() {
       <div className="center">
         <h2>Portfólio</h2>
         <div className="portfolio-container">
-          <a href="https://github.com/pedrosvr/E-Commerce-de-iPhones"><img src="/img/E-Commerce-de-iPhones" alt=""/></a>
+          <a href="https://github.com/pedrosvr/E-Commerce-de-iPhones"><img src="/img/E-Commerce-de-iPhones.png" alt=""/></a>
           <a href="https://github.com/pedrosvr/Personagens-de-Dragon-Ball-Super"><img src="/img/dragonball.png" alt=""/></a>
           <a href="https://github.com/pedrosvr/Whatslab"><img src="/img/whatslab.png" alt=""/></a>
           <a href="https://github.com/pedrosvr/Calculadora"><img src="/img/calculadora.png" alt=""/></a>
