165553c59af777e393545e779f01f8935ce096d3
pedrosvr <jopesaraiva0@gmail.com>
2022-12-02 19:19:17 -0300

    Quinto commit

--- components/About.js
+++ components/About.js
@@ -3,7 +3,7 @@ export default function About() {
     <section id="about">
       <div className="center">
         <h2>Sobre</h2>
-        <p>Sou um desenvolvedor de 19 anos apaixonado por tecnologia e encontrei na programação algo que eu realmente gosto de fazer, que é resolver problemas, receber novos desafios e estar sempre em busca de novos conhecimentos. Atualmente estou estudando as tecnologias de Back-end, como Node, Typescript, MySQL, AWS e FireBase na instituição de ensino Labenu.<br/>Por fim, pretendo ser um grande desenvolvedor e um grande solucionador de problemas.</p>
+        <p>Sou um desenvolvedor de 19 anos apaixonado por tecnologia e encontrei na programação algo que eu realmente gosto de fazer, que é resolver problemas, receber novos desafios e estar sempre em busca de novos conhecimentos. Atualmente estou estudando as tecnologias de Back-end, como Node, Typescript, MySQL, AWS e FireBase na instituição de ensino Labenu. Pretendo ser um grande desenvolvedor e um grande solucionador de problemas.<br/>Estou aberto a uma melhor apresentação, sinta-se à vontade para entrar em contato comigo pelo Linkedin, Instagram ou por outras formas que deixarei logo abaixo.</p>
       </div>
     </section>
   );
