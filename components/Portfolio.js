54cd65f52f2c404c5290fd962b59094d9bd2ccb9
pedrosvr <jopesaraiva0@gmail.com>
2023-01-07 20:37:01 -0300

    aAtualizando imagem da pokedex

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -5,10 +5,10 @@ export default function Portfolio() {
         <h2>Portfólio</h2>
         <div className="portfolio-container">
           <a href="https://github.com/pedrosvr/E-Commerce-de-iPhones"><img src="/img/E-Commerce-de-iPhones.png" alt=""/></a>
+          <a href="https://github.com/pedrosvr/projeto-react-apis"><img src="/img/pokedex.PNG" alt=""/></a>
           <a href="https://github.com/pedrosvr/Personagens-de-Dragon-Ball-Super"><img src="/img/dragonball.png" alt=""/></a>
           <a href="https://github.com/pedrosvr/Whatslab"><img src="/img/whatslab.png" alt=""/></a>
           <a href="https://github.com/pedrosvr/Calculadora"><img src="/img/calculadora.png" alt=""/></a>
-          <a href="https://github.com/pedrosvr/projeto-react-apis"><img src="/img/pokedex.PNG" alt=""/></a>
         </div>
       </div>
     </section>
